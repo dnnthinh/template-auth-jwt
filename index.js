72b2b362d522c56e544a05997605a82ada53e99c
dnnthinh <dnnthinh@gmail.com>
2020-02-26 21:53:26 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,10 +6,6 @@ const app = express();
 
 app.use(express.json());
 
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
-
 // connect to DB
 require('./config/database')
 
@@ -18,3 +14,7 @@ app.use('/api/user', authRouter);
 app.use('/api/posts', postRouter);
 
 app.use('*', (req, res) => res.send("404! Page not found!"));
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
