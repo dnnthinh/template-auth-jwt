d58b77a958c5dc2aee04415801f92c73ac6ded8b
dnnthinh <dnnthinh@gmail.com>
2020-02-26 22:11:16 +0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose');
 
 const db = mongoose.connect("mongodb+srv://udck:udck@trainingcluster-fn2to.mongodb.net/training", {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useMongoClient:true
+        useNewUrlParser: true
     }).then(db => {
         console.log("Connected correctly to mongo database!");
     })
