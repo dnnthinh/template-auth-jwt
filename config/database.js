32c0dc24eaa96bc201d96cc804c8c4dc26b058a5
dnnthinh <dnnthinh@gmail.com>
2020-02-26 22:30:30 +0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const db = mongoose.connect("mongodb+srv://udck:udck@trainingcluster-fn2to.mongodb.net/training", {
+const db = mongoose.connect(process.env.DB_MONGO_URL, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     }).then(db => {
